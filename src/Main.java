d757af4affaaddf1af7efc62279e454eabe8ed46
GediminasGsn <gasiunasgediminas@gmail.com>
2024-03-21 11:49:43 +0200

    ***

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String [] names = new String [10];
+        String [] surnames = new String [10];
+        int [] birthYears = new int [10];
+        boolean [] genders = new boolean [10];
+        int [][] grades = new int [10][10];
+        System.out.println(names[0] + " " + surnames [0] + " " + birthYears[0] + " " + genders[0] + " " + Arrays.toString(grades[0]));
+
+        Student studentas = new Student();
+
+        studentas.name = "Gediminas";
+        studentas.surname = "Gasiunas";
+        studentas.birthYear = 1999;
+        studentas.gender = true;
+        studentas.grades = new int []{5,6,8,9,5};
+
+        Student st2 = new Student();
+        st2.name = "Vardenis";
+        st2.surname = "Pavardenis";
+        st2.studentInfo();
+        studentas.studentInfo();
+
+        System.out.println(studentas.toString());
+
+
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Student {
+    public String name;
+    public String surname;
+    public int birthYear;
+    public boolean gender;
+    public int [] grades;
+    public void studentInfo(){
+        System.out.println("Labas as " + name + " " + surname);
+    }
+    @Override
+    public String toString() {
+        return this.name + " " +
+                this.surname + " " +
+                this.gender + " " +
+                this.birthYear + " " +
+                Arrays.toString(this.grades);
+    }
+
+}
